be2ad5096675368afbd7be77e9a560e5b143fc6e
Piotr Napieralski <piotrn-87@o2.pl>
2019-12-03 21:17:54 +0100

    Feat: Created Form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
-import "./App.css";
+import Form from "./Form";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="center blue-text">Lets get ready to rumble ... !!!</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        content: "Training React.js"
+      }
+    ]
+  };
+
+  deleteItems = items => {
+    const deleteItems = this.state.items.filter(item => {
+      return item.id !== items;
+    });
+  };
+
+  addItems = item => {
+    item.id = Math.random();
+    let newListItem = [...this.state.items, item];
+    this.setState({
+      items: newListItem
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="center blue-text">Lets get ready to rumble ... !!! </h1>
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label className="purple-text">Add some stuff</label>
+          <input type="text"></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
